4a3b1bbeeb0fc8f135d632ee0e35889ea6612898
Hemanth <ghshemanth@gmail.com>
2022-01-22 23:51:40 +0530

    Arrays

---
+++ Arrays/Arrays1.java
@@ -0,0 +1,19 @@
+public class Arrays1{
+    public static void main(String args[]){
+        // int[] arr = new int[3];
+        // int []arr = new int[3];
+        // int arr[] = new int[3];
+        int arr[] = {100,200,300};
+        // arr[0] = 10;
+        // arr[1] = 20;
+        // arr[2] = 30;
+
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
---
+++ Arrays/Arrays2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Arrays2{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the array size: ");
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+
+        System.out.println("Enter the arrays elements");
+
+        for(int i = 0; i < size; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("The array is: ");
+        for(int i = 0; i < size; i++){
+            System.out.println(arr[i]);
+        }
+
+    }
+}
